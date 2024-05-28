62a74524465378f69d388d1357270fdc009264ac
techlism <2021itb049.kundan@students.iiests.ac.in>
2024-05-28 12:02:34 +0530

    trying to adjust vertical gap

--- components/WidgetsContainer.tsx
+++ components/WidgetsContainer.tsx
@@ -5,7 +5,7 @@ import WeatherWidget from "./WeatherCard";
 
 export default function WidgetContainer(){
     return(
-        <div className="grid grid-cols-1 items-center gap-4 w-full">
+        <div className="grid grid-cols-1 items-center gap-6 w-full">
             <div className="flex border rounded-md p-2 align-middle justify-between items-center"> 
                 <WeatherWidget/>
                 <DarkModeSwitch/>
